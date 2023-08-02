a9f7dc4da93d7f7f3c77ce401fe5d221eb657351
Lumisumu <>
2023-08-02 22:02:31 +0300

    Check if imagelist exist and make a new one

--- README.md
+++ README.md
@@ -4,9 +4,8 @@ Python program for downloading and converting Pikmin 4 comics into PNG images.
 
 # How-to
 
-Paste links to imagelist.txt and run
-`python main.py`
+Paste links to imagelist.txt and run `python main.py`
 
 # To-do
 
-- Function to generate imagelist with links to comic
+- Code for automatic imagelist creation
--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def download_image(target_folder: str, name: str, url: str):
     image_link = rq.get(url)
     file_name = name + '.png'
 
-    # Make comic-folder it it doesn't exist yet
+    # Make cfolder it it does not exist
     if not os.path.exists(target_folder):
         os.makedirs(target_folder)
     
@@ -17,8 +17,22 @@ def download_image(target_folder: str, name: str, url: str):
         file.write(image_link.content)
         print(f'Image downloaded as "{file_name}".')
 
-
 if __name__ == '__main__':
+
+    # Make link list if it does not exist
+    if not os.path.exists("imagelist.txt"):
+        print("\n\nHello, welcome! imagelist.txt was not found, can I make one with Pikmin 4 urls for you?")
+        input("Press Enter to generate link list file...")
+
+        nf = open("imagelist.txt", "w")
+        #nf.write("url1\nurl2")
+        #To-do: check how many comics
+        #To-do: Loop for generating links and saving each onto new line in text file
+        
+        nf.close()
+
+        print("\nTask finished. Let's try downloading the images.")
+        input("Press Enter to start download...")
 
     # Open text file in project folder
     f = open("imagelist.txt", "r")
@@ -27,7 +41,7 @@ if __name__ == '__main__':
 
     # Loops the times of lines in the text file, each line is single url that is passed to download_image function
     for x in f:
-        print("\n Downloading from link:")
+        print("\nDownloading from link:")
         print(x)
         image_name = str(current_page)
         download_image('comic/', image_name, x.rstrip())
