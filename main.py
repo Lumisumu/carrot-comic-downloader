90ad0562fa856d9ae83c811a322989141390444d
Lumisumu <>
2023-08-02 20:27:09 +0300

    Add comments

--- main.py
+++ main.py
@@ -8,9 +8,11 @@ def download_image(target_folder: str, name: str, url: str):
     image_link = rq.get(url)
     file_name = name + '.png'
 
+    # Make comic-folder it it doesn't exist yet
     if not os.path.exists(target_folder):
         os.makedirs(target_folder)
     
+    # Save image to target folder
     with open(f'{target_folder}{file_name}', 'wb') as file:
         file.write(image_link.content)
         print(f'Image downloaded as "{file_name}".')
@@ -18,9 +20,12 @@ def download_image(target_folder: str, name: str, url: str):
 
 if __name__ == '__main__':
 
+    # Open text file in project folder
     f = open("imagelist.txt", "r")
+    # Counter to name pages
     current_page = 1
 
+    # Loops the times of lines in the text file, each line is single url that is passed to download_image function
     for x in f:
         print("\n Downloading from link:")
         print(x)
@@ -28,7 +33,7 @@ if __name__ == '__main__':
         download_image('comic/', image_name, x.rstrip())
         current_page += 1
         time.sleep(3)
-     
+    
     f.close()
 
     
