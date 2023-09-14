00fd704f4c343e549862763b79a5b6f193d568a9
Lumisumu <64976539+Lumisumu@users.noreply.github.com>
2023-09-14 15:43:05 +0300

    Edit printed lines, add comments

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def download_image(target_folder: str, name: str, image_address: str):
     # Save image to the folder
     with open(f'{target_folder}{file_name}', 'wb') as file:
         file.write(image_link.content)
-        print(f'Image downloaded as "{file_name}".')
+        print(f'Image download finished with name "{file_name}".')
 
 
 if __name__ == '__main__':
@@ -25,21 +25,25 @@ if __name__ == '__main__':
     # Check for imagelist.txt in the folder
     if os.path.exists("imagelist.txt"):
         print("Image link list found. Starting download...\n")
-        # Open text file in project folder
+
+        # Open text file
         f = open("imagelist.txt", "r")
 
         # Loops the times of lines in the text file, each line is single url that is passed to download_image function
         for x in f:
-            print("\nDownloading from link:")
-            print(x)
-            image_name = str("Pikmin-comic-" + current_page)
+            # Name for next download
+            image_name = str("Pik4-comic-" + current_page)
+            print("\nDownloading comic panel number " +
+                  current_page + " from link: " + x)
+            # Call download function
             download_image('comic/', image_name, x.rstrip())
             current_page += 1
+            # Wait time between downloads
             time.sleep(3)
 
+        # Close text file
         f.close()
+
     else:
         print(
             "ERROR: imagelist.txt missing, add file to this folder and run main.py again.\n")
-        # input("Press Enter to refresh.")
-        # TO-DO: try again
