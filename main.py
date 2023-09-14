9719df225446214eb536ab02764a9040944bfca9
Lumisumu <64976539+Lumisumu@users.noreply.github.com>
2023-09-14 20:22:16 +0300

    Add colors to print text, combine print text lines, add end print

--- main.py
+++ main.py
@@ -9,9 +9,25 @@ if __name__ == '__main__':
     image_name = str("Pik4-comic-1")
     target_folder = 'comic/'
 
+    # System call for colored printed text use in command prompt
+    os.system("")
+
+    # Printed text colors
+    class style():
+        BLACK = '\033[30m'
+        RED = '\033[31m'
+        GREEN = '\033[32m'
+        YELLOW = '\033[33m'
+        BLUE = '\033[34m'
+        MAGENTA = '\033[35m'
+        CYAN = '\033[36m'
+        WHITE = '\033[37m'
+        UNDERLINE = '\033[4m'
+        RESET = '\033[0m'
+
     # Check for imagelist.txt in the folder
     if os.path.exists("imagelist.txt"):
-        print("Image link list found. Starting download...\n")
+        print(style.RESET + "Image link list found. Starting download...\n")
 
         # Open text file
         f = open("imagelist.txt", "r")
@@ -19,14 +35,9 @@ if __name__ == '__main__':
         # Loops the times of lines in the text file, each line is single url that is passed to download_image function
         for x in f:
             # Name for next download
-            print("\nDownloading comic panel number ")
-            print(current_page)
-            print(" from link: ")
-            print(x)
-            # Call download function
-            # download_image('comic/', image_name, x.rstrip())
+            print(style.RESET + "Downloading comic panel number " +
+                  str(current_page) + " from link: " + str(x))
 
-            # DOWNLOAD START
             image_link = rq.get(x.rstrip())
             file_name = image_name + '.png'
 
@@ -37,8 +48,8 @@ if __name__ == '__main__':
             # Save image to the folder
             with open(f'{target_folder}{file_name}', 'wb') as file:
                 file.write(image_link.content)
-                print(f'Image download finished with name "{file_name}".')
-            # DOWNLOAD END
+                print(style.GREEN +
+                      f'Image download finished with name "{file_name}".\n' + style.RESET)
 
             current_page += 1
             image_name = str("Pik4-comic-")
@@ -48,6 +59,7 @@ if __name__ == '__main__':
 
         # Close text file
         f.close()
+        print(style.RESET + 'Download complete, check your "comic" folder.')
 
     else:
         print(
