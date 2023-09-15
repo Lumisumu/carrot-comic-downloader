cff172e78d375cd625908d88b09efd315525e889
Lumisumu <64976539+Lumisumu@users.noreply.github.com>
2023-09-15 20:34:47 +0300

    Begin work on new list generation function

--- main.py
+++ main.py
@@ -3,6 +3,46 @@ import os
 import time
 
 
+def generate_image_list():
+
+    comic_amount = 45
+
+    print('\nAutomatic image list generation is supported up to comic #45: "The Captains Treasure". If you want the automatic generation to go beyond this, you can input the number of latest comic.')
+
+    print("\nAutomatic option: Input " + style.GREEN + "A" +
+          style.RESET + " and press Enter to use automatic generation. ")
+    print('Manual option: Input ' + style.YELLOW + 'latest comic number' + style.RESET +
+          ', for example "52" to use manual amount generation.')
+    amount_style = input()
+
+    if amount_style == "A":
+        print("Generating automatic imagelist.txt")
+    else:
+        print("Generating manual imagelist.txt")
+        comic_amount = int(amount_style)
+
+    nf = open("imagelist.txt", "w")
+
+    # Generate images in a loop
+    i = 1
+    while i <= int(comic_amount):
+        if i <= 5:
+            nf.write("Link1" + "\n")
+        if i == 6:
+            nf.write("Link2" + "\n")
+        if i > 6 and i < 10:
+            nf.write("Link3" + "\n")
+        if i >= 10 and i < 30:
+            nf.write("Link4" + "\n")
+        if i >= 30:
+            nf.write("Link5" + "\n")
+        i += 1
+
+    nf.close()
+
+    input("Task finished. Press Enter to start download...")
+
+
 if __name__ == '__main__':
 
     current_page = 1
@@ -24,6 +64,18 @@ if __name__ == '__main__':
         WHITE = '\033[37m'
         UNDERLINE = '\033[4m'
         RESET = '\033[0m'
+
+    print("Do you want to use automatic image list generation?")
+    print("Type " + style.GREEN + "Y" + style.RESET +
+          " or " + style.RED + "N" + style.RESET)
+    list_choice = input()
+
+    if list_choice == "Y":
+        if not os.path.exists("imagelist.txt"):
+            generate_image_list()
+        if os.path.exists("imagelist.txt"):
+            input(
+                "Image list file already exists. Press Enter to continue with existing file...")
 
     # Check for imagelist.txt in the folder
     if os.path.exists("imagelist.txt"):
