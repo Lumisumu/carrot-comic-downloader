2887f79f07e327c36aeefce6a176e19d5d481ac5
Lumisumu <64976539+Lumisumu@users.noreply.github.com>
2023-10-02 18:52:15 +0300

    Separate link writing to it's own script

--- main.py
+++ main.py
@@ -2,47 +2,7 @@ import requests as rq
 import os
 import time
 
-
-def write_links(comic_number: int):
-
-    nf = open("imagelist.txt", "a")
-
-    # Url format for the images has changed multiple times, this part determines which url format to use and writes the link correctly
-    if comic_number == 19 or comic_number == 37:
-        print("Skipping unused comic number #" + str(comic_number) + ". This is normal and not an error. No comic can be found at this number.")
-
-    elif comic_number == 5:
-        i = 1
-        while i < 6:
-            nf.write("https://assets.nintendo.com/image/upload/w_600,f_auto,q_auto/v1686932568/Microsites/PIKMIN-Portal/comics/00" + str(comic_number) + "/pikmin-comic-00" + str(comic_number) + "_0" + str(i))
-            i += 1
-            nf.write("\n")
-
-    elif comic_number < 10:
-        i = 1
-        while i < 6:
-            nf.write("https://assets.nintendo.com/image/upload/w_600,f_auto,q_auto/v1686959530/Microsites/PIKMIN-Portal/comics/00" + str(comic_number) + "/EN/nint2402-pikmin4-manga00" + str(comic_number) + "_0" + str(i))
-            i += 1
-            nf.write("\n")
-
-    elif comic_number >= 10 and comic_number < 30:
-        i = 1
-        while i < 6:
-            nf.write("https://assets.nintendo.com/image/upload/w_600,f_auto,q_auto/v1688141442/Microsites/PIKMIN-Portal/comics/0" + str(comic_number) + "/EN/nint2402-pikmin4-manga0" + str(comic_number) + "_0" + str(i))
-            i += 1
-            nf.write("\n")
-
-    elif comic_number >= 30:
-        i = 1
-        while i < 6:
-            nf.write("https://assets.nintendo.com/image/upload/w_600,f_auto,q_auto/v1694014678/Microsites/PIKMIN-Portal/comics/" + str(comic_number) + "/nint2402-pikmin4-manga0" + str(comic_number) + "_0" + str(i))
-            i += 1
-            nf.write("\n")
-
-    else:
-        print(style.RED + "Error #3: Writing to text file failed." + style.RESET)
-
-    nf.close()
+import scripts.CarrotScript as carrot
 
 
 def generate_image_list():
@@ -72,7 +32,7 @@ def generate_image_list():
     # Loop through panel numbers and send them one by one to write_links function
     i = 1
     while i <= int(comic_amount):
-        write_links(i)
+        carrot.write_links(i)
         i += 1
 
     input("Task finished. Press Enter to start download...")
@@ -80,7 +40,7 @@ def generate_image_list():
 
 def download_images():
 
-    target_folder = 'comic/'
+    target_folder = 'output/'
     current_page = 1
     image_name = str("Pik4-comic-1")
 
---
+++ scripts/CarrotScript.py
@@ -0,0 +1,42 @@
+import os
+
+def write_links(comic_number: int):
+
+    nf = open("imagelist.txt", "a")
+
+    # Url format for the images has changed multiple times, this part determines which url format to use and writes the link correctly
+    if comic_number == 19 or comic_number == 37:
+        print("Skipping unused comic number #" + str(comic_number) + ". This is normal and not an error. No comic can be found at this number.")
+
+    elif comic_number == 5:
+        i = 1
+        while i < 6:
+            nf.write("https://assets.nintendo.com/image/upload/w_600,f_auto,q_auto/v1686932568/Microsites/PIKMIN-Portal/comics/00" + str(comic_number) + "/pikmin-comic-00" + str(comic_number) + "_0" + str(i))
+            i += 1
+            nf.write("\n")
+
+    elif comic_number < 10:
+        i = 1
+        while i < 6:
+            nf.write("https://assets.nintendo.com/image/upload/w_600,f_auto,q_auto/v1686959530/Microsites/PIKMIN-Portal/comics/00" + str(comic_number) + "/EN/nint2402-pikmin4-manga00" + str(comic_number) + "_0" + str(i))
+            i += 1
+            nf.write("\n")
+
+    elif comic_number >= 10 and comic_number < 30:
+        i = 1
+        while i < 6:
+            nf.write("https://assets.nintendo.com/image/upload/w_600,f_auto,q_auto/v1688141442/Microsites/PIKMIN-Portal/comics/0" + str(comic_number) + "/EN/nint2402-pikmin4-manga0" + str(comic_number) + "_0" + str(i))
+            i += 1
+            nf.write("\n")
+
+    elif comic_number >= 30:
+        i = 1
+        while i < 6:
+            nf.write("https://assets.nintendo.com/image/upload/w_600,f_auto,q_auto/v1694014678/Microsites/PIKMIN-Portal/comics/" + str(comic_number) + "/nint2402-pikmin4-manga0" + str(comic_number) + "_0" + str(i))
+            i += 1
+            nf.write("\n")
+
+    else:
+        print(style.RED + "Error #3: Writing to text file failed." + style.RESET)
+
+    nf.close()
