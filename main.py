4061414cff1cbd1564d851991c653d4d85788571
Lumisumu <64976539+Lumisumu@users.noreply.github.com>
2023-11-08 22:10:17 +0200

    Support up to 67

--- main.py
+++ main.py
@@ -176,7 +176,7 @@ if __name__ == '__main__':
         case "1":
             comic_choice = "Pikmin 4 comic"
             comic_name_format = "Pikmin 4 comic"
-            last_comic = 66
+            last_comic = 67
 
         case "2":
             comic_choice = "DLC"
