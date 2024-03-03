38befd4a00b375cc23cea62c000cfcc71520467d
Lumisumu <64976539+Lumisumu@users.noreply.github.com>
2024-03-03 19:02:08 +0200

    Support update

--- main.py
+++ main.py
@@ -164,12 +164,12 @@ if __name__ == '__main__':
         case "1":
             comic_choice = "Pikmin 4 comic"
             comic_name_format = "Pikmin 4 comic"
-            last_comic = 111
+            last_comic = 115
 
         case "2":
             comic_choice = "DLC"
             comic_name_format = "DLC"
-            last_comic = 893
+            last_comic = 894
 
         case "0":
             comic_choice = "Custom"
