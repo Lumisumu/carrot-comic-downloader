c40271c57c1ab4704c287a7974f21bdbd3c27840
Lumisumu <64976539+Lumisumu@users.noreply.github.com>
2024-03-25 18:20:59 +0200

    Support update

--- main.py
+++ main.py
@@ -149,7 +149,7 @@ if __name__ == '__main__':
     # Comic default options, update newest comic number here
     comics = {
     "1": ("Pikmin 4 comic", "Pikmin 4 comic" , 123),
-    "2": ("DLC", "DLC", 896),
+    "2": ("DLC", "DLC", 897),
     "0": ("Custom", "Custom comic", None)
     }
 
